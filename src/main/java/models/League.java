b0238e8bbcfb7ad610abde0e61fdfc892347fc49
Kelsie Braidwood <kelsieiona@gmail.com>
2018-05-18 23:08:48 +0100

    adds league enums

---
+++ src/main/java/models/League.java
@@ -0,0 +1,12 @@
+package models;
+
+public enum League {
+    MENS_PREMIER,
+    WOMENS_PREMIER,
+    SVL_WOMENS1,
+    SVL_WOMENS2,
+    SVL_WOMENS3,
+    SVL_MENS1,
+    SVL_MENS2,
+    SVL_MENS3
+}
